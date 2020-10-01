734c0f4b887fd8f5988dc8cb5e299d556bbd8bad
Leonidios <programertv633@gmail.com>
2020-10-01 22:09:56 +0300

    imporved rawing app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,28 +2,36 @@
 // import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 import React,{useEffect,useState} from "react"
 import tmp from './tmp.json';
+import "./pages/base/base.css"
+import {Form} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // https://www.pornhub.com/view_video.php?viewkey=ph5f06e9657a805#1
 
 
 function App() {
   const [drawing,setDrawing] = useState(false);
-  const [diff,setDiff] = useState(10)
+  const [LineWidth,setLineWidth] = useState(10);
+  const [caligraphy,setCaligrapgy] = useState(0);
+  const [color,setColor] = useState("#000000")
   const [paths,setPaths] = useState([])
 
   useEffect(() => {
     const canvas = document.getElementById("canvas");
     canvas.setAttribute("tabindex", 0);
-    let ctx = canvas.getContext("2d");
-    ctx.lineCap = "round";
-    ctx.lineWidth = diff;
-    // ctx.moveTo(e.clientX-diff,e.clientY-diff);
+    // let ctx = canvas.getContext("2d");
+    // ctx.lineCap = "round";
+    // ctx.lineWidth = diff;
+    // // ctx.moveTo(e.clientX-diff,e.clientY-diff);
 
-    tmp.forEach(cords => {
-      ctx.lineTo(...cords)
-      ctx.stroke();
-      ctx.beginPath();  
-    })
+    // tmp.forEach(cords => {
+    //   ctx.lineTo(...cords)
+    //   ctx.stroke();
+    //   ctx.beginPath();  
+    // })
+    
+
   })
 
   const handleMove = (e) => { 
@@ -31,29 +39,58 @@ function App() {
     if(!drawing) return null;
     let ctx = canvas.getContext("2d");
     ctx.lineCap = "round";
-    ctx.lineWidth = diff;
-    let [x,y] = [e.clientX,e.clientY]
+    ctx.lineWidth = LineWidth;
+    ctx.strokeStyle  = color;
+    let [x,y] = [e.clientX,e.clientY-30]
     // ctx.moveTo(e.clientX-diff,e.clientY-diff);
     ctx.lineTo(x,y);
     setPaths(prev => [...prev,[x,y]])
     ctx.stroke();
     ctx.beginPath();
+    ctx.moveTo(x-caligraphy,y-caligraphy)
     // console.log(paths)
   }
 
   const handleKeyPress = (e) => {
     if(e.keyCode===65){
-      setDiff(prev => prev +1)
+      setLineWidth(prev => prev +1)
     }
     else if(e.keyCode==68){
-      setDiff(prev=>prev-1)
+      setLineWidth(prev=>prev-1)
+    }
+    else if (e.keyCode===87){
+      setCaligrapgy(prev => prev +1)
+    }
+    else if (e.keyCode===83){
+      setCaligrapgy(prev => prev-1)
+    }
+    else if (e.keyCode===67){
+      let pick = document.getElementById("pick");
+      pick.style.visibility = "visible";
+      pick.click();pick.style.visibility = "hidden"
     }
   }
 
   return (
     <div>
-      <canvas onKeyDown={e => handleKeyPress(e)} onMouseUp={() =>  setDrawing(false)} onMouseDown={() => setDrawing(true)} onMouseMove={(e) => {handleMove(e)}} width={800} height={800} id="canvas"></canvas>
-      <button onClick={() => console.log(paths)}>printf</button>
+      <div>
+        hello world
+      </div>
+      <canvas className={"cs"} style={{"float" : "left",cursor : "crosshair"}} onKeyDown={e => handleKeyPress(e)}
+       onMouseUp={() =>  {setDrawing(false);document.getElementById("canvas").getContext("2d").beginPath()}} 
+       onMouseDown={() => setDrawing(true)} 
+       onMouseMove={(e) => {handleMove(e)}} width={800} height={500} id="canvas"></canvas>
+      <div style={{"float" : "right",resize : "none",marginRight : "50px"}}>
+        <div className={"css"} style={{height : "500px",width : "200px",backgroundColor : "transparent"}}>
+        </div>
+        <Form.Group style={{"marginTop" : "30px"}}  controlId="exampleForm.ControlTextarea1">
+          <Form.Control style={{"resize" : "none","maxHeight" : "50px"}} as="textarea" rows="3" />
+        </Form.Group>
+      </div>
+      <div>
+        <input onChange={(e) => {setColor(e.target.value)}} id="pick" style={{"backgroundColor" : "transparent",border : "0",height : "0px",width : "0px",position : "fixed","visibility" : "hidden"}} type="color"></input>
+      </div>
+      {/* <button onClick={() => console.log(paths)}>printf</button> */}
     </div>
   );
 }
